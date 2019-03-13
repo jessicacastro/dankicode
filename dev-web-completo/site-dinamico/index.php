702ee9b6adb8fbe40e2a804b5f047c85a865087b
Jessica Castro <jessica.castros@outlook.com>
2019-03-13 14:58:22 -0300

    Criação de projeto de site dinâmico

---
+++ dev-web-completo/site-dinamico/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width-device=1.0">
+	<title>Site Dinâmico - Danki Code</title>
+</head>
+<body>
+
+</body>
+</html>
