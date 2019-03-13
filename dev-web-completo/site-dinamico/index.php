9e35c84a03a2bea815a78af2758f583f887cbfe6
Jessica Castro <jessica.castros@outlook.com>
2019-03-13 15:12:07 -0300

    estilo css e marcação html da pagina

--- dev-web-completo/site-dinamico/index.php
+++ dev-web-completo/site-dinamico/index.php
@@ -1,11 +1,103 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width-device=1.0">
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<meta name="keywords" content="palavras-chave, do, meu, site" />
+	<meta name="description" content="Site feito para o curso do Danki Code" />
 	<title>Site Dinâmico - Danki Code</title>
 </head>
 <body>
+
+	<header>
+		<div class="center">
+			<div class="logo"></div><!--logo-->
+			<nav class="desktop">
+				<ul>
+					<li><a href="#">Home</a></li>
+					<li><a href="#">Sobre</a></li>
+					<li><a href="#">Serviços</a></li>
+					<li><a href="#">Contato</a></li>
+				</ul>
+			</nav><!--desktop-->
+
+			<nav class="mobile">
+				<ul>
+					<li><a href="#">Home</a></li>
+					<li><a href="#">Sobre</a></li>
+					<li><a href="#">Serviços</a></li>
+					<li><a href="#">Contato</a></li>
+				</ul>
+			</nav><!--mobile-->
+		</div><!--center-->
+	</header>
+
+	<section class="banner-principal">
+		<div class="center">
+			<form>
+				<h2>Qual o seu melhor e-mail?</h2>
+				<input type="email" name="email" required />
+				<input type="submit" name="acao" value="Cadastrar" />
+			</form>
+		</div><!--center-->
+	</section><!--banner-principal-->
+
+	<section class="descricao-autor">
+		<div class="center">
+			<div class="w50">
+				<h2>Jessica Castro P. Silva</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			</div><!--w50-->
+
+			<div class="w50">
+				<img src="img/foto.jpg">
+			</div>
+		</div><!--center-->
+	</section><!--descricao-autor-->
+
+	<section class="especialidades">
+		<h2 class="title">Especialidades</h2>
+		<div class="center">
+			<div class="box-especialidade">
+				<h3>Icone 1</h3>
+				<h3>CSS3</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat.</p>
+			</div><!--box-especialidade-->
+
+			<div class="box-especialidade">
+				<h3>Icone 2</h3>
+				<h3>HTML5</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat.</p>
+			</div><!--box-especialidade-->
+
+			<div class="box-especialidade">
+				<h3>Icone 3</h3>
+				<h3>JavaScript</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat.</p>
+			</div><!--box-especialidade-->
+		</div><!--center-->
+	</section><!--especialidades-->
 
 </body>
 </html>
